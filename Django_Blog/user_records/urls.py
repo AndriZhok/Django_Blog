c4d475388dfc40ccdea4ee188867403de82ca051
AndriZhok <andri195595@gmail.com>
2024-05-16 10:26:06 +0300

    l

---
+++ Django_Blog/user_records/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'user_records'
+urlpatterns = [
+    # Головна сторінка
+    path('', views.index, name='index'),
+    # Сторінка що відображає всі теми
+    path('topics/', views.topics, name='topics'),
+    # Сторінка призначена окремій темі
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Сторінка для додавання нової теми
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Сторінка для додавання нового допису
+    path('new_entry/<int:topic_id>/',views.new_entry, name='new_entry'),
+    # Сторінка для редагування допису
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+]
--- Django_Blog/user_records/views.py
+++ Django_Blog/user_records/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'user_records/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics' : topics}
+    return render(request, 'user_records/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'user_records/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # Жодних даних не відправлено; створити порожню форму
+        form = TopicForm()
+    else:
+        # відправляємо POST; обробити дані.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_records:topics')
+
+    # Показати порожню або не дійсну форму
+    context = {'form' : form}
+    return render(request, 'user_records/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('user_records:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'user_records/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_records:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'user_records/edit_entry.html', context)
